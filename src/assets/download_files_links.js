82f7a74dffe56e8ec0d03740893a7d6b0757dd8c
Nir Gilad <nir@vacayz.com>
2021-09-20 18:12:44 +0300

    tweak how files are stored and linked in app. add resume file.

---
+++ src/assets/download_files_links.js
@@ -0,0 +1,7 @@
+export const THESIS_SLIDES =
+  "https://github.com/Nirol/portfolio/raw/main/src/assets/thesis/Thesis_SlideShow.ppsx";
+
+export const THESIS_PDF = "to_complete";
+
+export const RESUME_ML =
+  "https://github.com/Nirol/portfolio/raw/main/src/assets/resume/Resume_ML_Nir_Gilad.pdf";
